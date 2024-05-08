852e0370c85b222e12a0594b6dbfc48a6797ce89
Dauren <dauren@daurenmac.local>
2024-05-08 18:51:53 +0500

    Sort slice in slice

--- map1.go
+++ map1.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	m := make(map[string][]string)
@@ -12,4 +15,11 @@ func main() {
 	arr2 := []string{"yyyy", "ppp"}
 	result := append(arr, arr2...)
 	fmt.Println(result)
+
+	g := [][]int{{1, 2}, {3, 4}, {1, 3}}
+	sort.Slice(g, func(i, j int) bool {
+		return g[i][0] < g[j][0]
+	})
+
+	fmt.Println(g)
 }
